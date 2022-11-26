823fe09103f1f1dfe5be0c51c2dce0da2562095b
sudanvannin <83932695+sudanvannin@users.noreply.github.com>
2022-11-26 15:46:41 -0300

    Add files via upload

---
+++ candidato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_poo_puc
+{
+    public class Candidato
+    {
+        public Candidato(int id, string nome, int qtdVotos, Partido partido)
+        {
+            Id = id;
+            Nome = nome;
+            QtdVotos = qtdVotos;
+            Partido = partido;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int QtdVotos { get; set; }
+        public Partido Partido { get; set; }
+    }
+}
