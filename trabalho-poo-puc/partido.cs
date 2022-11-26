983e77501f5d6e5df2322dd9defc9cf94e3dcbf5
Felipe Sudan <vanninsudan@gmail.com>
2022-11-26 15:50:26 -0300

    Create partido.cs

---
+++ trabalho-poo-puc/partido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_poo_puc
+{
+    public class Partido
+    {
+        public Partido(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
